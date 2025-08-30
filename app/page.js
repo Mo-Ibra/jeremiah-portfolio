50acfc3da5f7a0a6c04adf37e554fa8af8ccc8b2
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-08-30 14:04:47 +0300

    Added Case Studies

--- app/page.js
+++ app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
       <VideoReels />
       <ServicesCarousel />
       <Portfolio />
-      {/* <CaseStudies /> */}
+      <CaseStudies />
       <About />
       <Principles />
       <DTCComparison />
