664746e4b659c502bf7242ca8d9e732029877d55
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-08-30 16:27:23 +0300

    Fix Faqs

--- components/Faqs.js
+++ components/Faqs.js
@@ -7,29 +7,26 @@ import { faqData } from "@/data/data";
 const Faqs = () => {
   return (
     <section id="faqs" className="faqs-section">
-      <div className="faqs-container">
-        <h2
-          className="faqs-title"
-        >
-          <span className="f-bold">Questions</span>
-          <span className="f-thin"> you might have</span>
-        </h2>
-
-        <div
-          className="faq-list"
-        >
-          {faqData.map((faq, index) => (
-            <div key={index} className="faq-item">
-              <input type="checkbox" id={`faq${index}`} />
-              <label className="faq-question" htmlFor={`faq${index}`}>
-                <span className="faq-index">
-                  {String(index + 1).padStart(2, "0")}
-                </span>
-                <span>{faq.question}</span>
-              </label>
-              <div className="faq-answer">{faq.answer}</div>
-            </div>
-          ))}
+      <div className="container">
+        <div className="faqs-container">
+          <h2 className="faqs-title">
+            <span className="f-bold">Questions</span>
+            <span className="f-thin"> you might have</span>
+          </h2>
+          <div className="faq-list">
+            {faqData.map((faq, index) => (
+              <div key={index} className="faq-item">
+                <input type="checkbox" id={`faq${index}`} />
+                <label className="faq-question" htmlFor={`faq${index}`}>
+                  <span className="faq-index">
+                    {String(index + 1).padStart(2, "0")}
+                  </span>
+                  <span>{faq.question}</span>
+                </label>
+                <div className="faq-answer">{faq.answer}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
